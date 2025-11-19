045d7be03e37b3f895e69734683fe5f71b0f7407
WAEL Zeyad OBS/OINIS <BDBK8711@orange.com>
2025-11-19 14:50:41 +0200

    Edit Gcquery and changes in plugin

--- src/components/GCQueryEditor.tsx
+++ src/components/GCQueryEditor.tsx
@@ -7,7 +7,6 @@ import { GCInput } from "./GCInput";
 import { defaultQuery } from "../defaults";
 import { GCDataSourceOptions, GCQuery } from "../types";
 import { DataSource } from "../datasource";
-import { getBackendSrv } from "@grafana/runtime";
 
 const { FormField } = LegacyForms;
 
@@ -16,19 +15,16 @@ type Props = QueryEditorProps<DataSource, GCQuery, GCDataSourceOptions>;
 interface State {
   appOptions: Array<SelectableValue<number>>;
   loadingApps: boolean;
-  networkOptions: Array<SelectableValue<string>>;
 }
 
 export class GCQueryEditor extends PureComponent<Props, State> {
   state: State = {
     appOptions: [],
     loadingApps: false,
-    networkOptions: [],
   };
 
   componentDidMount() {
     this.loadApps();
-    this.loadNetworks(); // optional network dropdown
   }
 
   async loadApps() {
@@ -37,13 +33,14 @@ export class GCQueryEditor extends PureComponent<Props, State> {
       const url = `${this.props.datasource.url}/fastedge/v1/apps`;
       const apiKey = this.props.datasource.instanceSettings.secureJsonData?.apiKey ?? "";
 
-      const response = await getBackendSrv().datasourceRequest({
+      const response = await (await import("@grafana/runtime")).getBackendSrv().datasourceRequest({
         method: "GET",
         url,
         headers: { Authorization: `apikey ${apiKey}` },
       });
 
       const rawData = response?.data;
+
       const apps: any[] = Array.isArray(rawData)
         ? rawData
         : Array.isArray((rawData as any)?.apps)
@@ -57,30 +54,7 @@ export class GCQueryEditor extends PureComponent<Props, State> {
 
       this.setState({ appOptions, loadingApps: false });
     } catch (error) {
-      console.error("Failed to load apps:", error);
       this.setState({ appOptions: [], loadingApps: false });
-    }
-  }
-
-  async loadNetworks() {
-    try {
-      const url = `${this.props.datasource.url}/fastedge/v1/networks`;
-      const apiKey = this.props.datasource.instanceSettings.secureJsonData?.apiKey ?? "";
-
-      const response = await getBackendSrv().datasourceRequest({
-        method: "GET",
-        url,
-        headers: { Authorization: `apikey ${apiKey}` },
-      });
-
-      const rawData = response?.data;
-      const networks: any[] = Array.isArray(rawData) ? rawData : [];
-
-      const networkOptions = networks.map((n: string) => ({ label: n, value: n }));
-      this.setState({ networkOptions });
-    } catch (error) {
-      console.error("Failed to load networks:", error);
-      this.setState({ networkOptions: [] });
     }
   }
 
@@ -106,7 +80,7 @@ export class GCQueryEditor extends PureComponent<Props, State> {
   render() {
     const query: GCQuery = defaults(this.props.query, defaultQuery);
     const { id = 0, step = 60, network = "" } = query;
-    const { appOptions, loadingApps, networkOptions } = this.state;
+    const { appOptions, loadingApps } = this.state;
 
     const inputStyle: React.CSSProperties = {
       width: "100%",
@@ -118,11 +92,18 @@ export class GCQueryEditor extends PureComponent<Props, State> {
     };
 
     const selectedApp = appOptions.find((opt) => opt.value === id);
-    const selectedNetwork = networkOptions.find((opt) => opt.value === network);
 
     return (
-      <div style={{ display: "flex", flexDirection: "column", gap: "16px", width: "100%", maxWidth: "420px", padding: "12px" }}>
-        {/* App Name */}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "16px",
+          width: "100%",
+          maxWidth: "420px",
+          padding: "12px",
+        }}
+      >
         <FormField
           label="App Name"
           labelWidth={12}
@@ -141,10 +122,8 @@ export class GCQueryEditor extends PureComponent<Props, State> {
           }
         />
 
-        {/* Step */}
         <FormField
           label="Step"
-          tooltip="Time series step in seconds"
           labelWidth={12}
           inputEl={
             <GCInput
@@ -157,29 +136,17 @@ export class GCQueryEditor extends PureComponent<Props, State> {
           }
         />
 
-        {/* Network */}
         <FormField
           label="Network"
-          tooltip="Optional network filter"
           labelWidth={12}
           inputEl={
-            networkOptions.length > 0 ? (
-              <Select
-                options={networkOptions}
-                value={selectedNetwork}
-                onChange={(opt) => this.onNetworkChange(opt.value ?? "")}
-                placeholder="Select network"
-                width={40}
-              />
-            ) : (
-              <GCInput
-                type="text"
-                value={network}
-                onChange={this.onNetworkChange}
-                placeholder="Enter network name"
-                style={inputStyle}
-              />
-            )
+            <GCInput
+              type="text"
+              value={network}
+              onChange={this.onNetworkChange}
+              placeholder="Enter network name"
+              style={inputStyle}
+            />
           }
         />
       </div>
